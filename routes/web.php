8946f580d5a921dc3f23711054fa2ffffa06d36a
George Pipkin <gpp8p@Virginia.EDU>
2020-01-10 22:35:26 -0500

    fixing problem with no cards in getLayoutById

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/cardinstances', 'cardInstanceController@getLayoutCardInstances')->name('cardinstances');
-Route::get('/blankLayout', 'LayoutController@createBlankLayout')->name('blanklayout');
-Route::post('/createLayout', 'LayoutController@createNewLayout')->name('newlayout');
+//Route::get('/cardinstances', 'cardInstanceController@getLayoutCardInstances')->name('cardinstances');
+//Route::get('/blankLayout', 'LayoutController@createBlankLayout')->name('blanklayout');
+//Route::post('/createLayout', 'LayoutController@createNewLayout')->name('newlayout');
 Route::post('/createLayoutNoBlanks', 'LayoutController@createNewLayoutNoBlanks')->name('newlayoutNoBlanks');
 Route::post('/saveCard', 'cardInstanceController@saveCard')->name('saveCard');
 Route::post('/saveCardOnly', 'cardInstanceController@saveCardOnly')->name('saveCardOnly');
 Route::get('/getLayout', 'cardInstanceController@getLayoutById')->name('getLayout');
-Route::post('/saveCard', 'cardInstanceController@saveCard')->name('saveCard');
-Route::get('/csrfTest', 'cardInstanceController@getCsrf')->name('csrfTest');
-Route::get('/serveTest', 'cardInstanceController@serveTest')->name('serveTest');
+//Route::post('/saveCard', 'cardInstanceController@saveCard')->name('saveCard');
+//Route::get('/csrfTest', 'cardInstanceController@getCsrf')->name('csrfTest');
+//Route::get('/serveTest', 'cardInstanceController@serveTest')->name('serveTest');
 Route::get('/layoutList', 'LayoutController@getLayoutList')->name('layoutList');
 header('Access-Control-Allow-Origin:  *');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
